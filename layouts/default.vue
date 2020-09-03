62aa8cc4a44e65cc690b726d697733183f24d124
Quardruple <jokeretaaye@gmail.com>
2020-09-03 17:37:53 +0700

    - Start Project

---
+++ layouts/default.vue
@@ -0,0 +1,143 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <nav id="sidenav-main" class="navbar navbar-sidebar" background-color="vue" short-title="Argon" title="Argon">
+        <div class="container-fluid" style="padding-left:0px;">
+          <ul class="navbar-nav">
+            <li class="nav-item" v-for="(item, i) in listUrl" :key="i" :to="item.to" router exact>
+              <nuxt-link class="nav-link" :to="item.to">{{ item.title }}</nuxt-link>
+            </li>
+            <!-- <li class="nav-item">
+              <a href="/icons" class="nav-link"><i class="ni ni-planet text-blue"></i><span class="nav-link-text">Icons</span></a>
+            </li>
+            <li class="nav-item">
+              <a href="/maps" class="nav-link"><i class="ni ni-pin-3 text-orange"></i><span class="nav-link-text">Maps</span></a>
+            </li>
+            <li class="nav-item">
+              <a href="/profile" class="nav-link"><i class="ni ni-single-02 text-yellow"></i><span class="nav-link-text">User Profile</span></a>
+            </li>
+            <li class="nav-item">
+              <a href="/tables" class="nav-link"><i class="ni ni-bullet-list-67 text-red"></i><span class="nav-link-text">Tables</span></a>
+            </li>
+            <li class="nav-item">
+              <a href="/login" class="nav-link"><i class="ni ni-key-25 text-info"></i><span class="nav-link-text">Login</span></a>
+            </li>
+            <li class="nav-item">
+              <a href="/register" class="nav-link"><i class="ni ni-circle-08 text-pink"></i><span class="nav-link-text">Register</span></a>
+            </li> -->
+          </ul>
+          <!-- <hr class="my-3">
+          <h6 class="navbar-heading text-muted">Documentation</h6>
+          <ul class="navbar-nav mb-md-3">
+            <li class="nav-item">
+              <a href="https://demos.creative-tim.com/vue-argon-dashboard/documentation" class="nav-link"><i class="ni ni-spaceship"></i> Getting started </a>
+            </li>
+            <li class="nav-item">
+              <a href="https://demos.creative-tim.com/vue-argon-dashboard/documentation/foundation/colors.html" class="nav-link"><i class="ni ni-palette"></i> Foundation </a>
+            </li>
+            <li class="nav-item">
+              <a href="https://demos.creative-tim.com/vue-argon-dashboard/documentation/components/alerts.html" class="nav-link"><i class="ni ni-ui-04"></i> Components </a>
+              </li>
+          </ul> -->
+        </div>
+      </nav>
+    </div>
+
+    <div class="content-page">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    console.log(this.$router.currentRoute.path);
+    return {
+      clipped: true,
+      drawer: true,
+      fixed: false,
+      listUrl: [
+        {
+          icon: 'mdi-apps',
+          title: 'Dashboard',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Pos',
+          to: '/pos'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Goods',
+          to: '/ListGoods'
+        }
+      ],
+      miniVariant: true,
+      right: true,
+      rightDrawer: false,
+      title: ''
+    }
+  },
+  computed: {
+    header () {
+      return this.$t('header')
+    },
+    availableLocales () {
+      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
+    }
+  }
+}
+</script>
+<style>
+.bg-white {
+  background-color: #fff !important;
+}
+
+@media (min-width: 768px) {
+  .navbar-sidebar {
+    left: 0;
+    border-width: 0 1px 0 0;
+    display: block;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    max-width: 250px;
+    padding-left: 0rem;
+    padding-right: 1.5rem;
+    overflow-y: auto;
+    box-shadow: 0 0 2rem 0 rgba(136, 152, 170, 0.15) !important;
+    background-color: #fff !important;
+  }
+
+  .navbar-nav {
+    width: 100%;
+  }
+  
+  .navbar-nav .nav-link {
+    padding-left: 1rem;
+    padding-right: 1rem;
+    font-size: 0.9rem;
+  }
+
+  .nuxt-link-exact-active {
+    top: 0.25rem;
+    bottom: 0.25rem;
+    left: 0;
+    right: auto;
+    border-left: 3px solid #5e72e4;
+    background: rgba(0,0,0,0.04);
+    border-bottom: 0;
+    width: 100%;
+    color: rgba(0, 0, 0, 0.9);
+  }
+  /* .page-enter-active, .page-leave-active {
+    transition: all 0.5s;
+  }
+  .page-enter, .page-leave {
+    opacity: 0;
+    transform: scale();
+  } */
+}
+</style>
---
+++ pages/ListGoods.vue
@@ -0,0 +1,259 @@
+<template>
+  <div class="container-fluid mt--7">
+    <div class="row">
+      <div class="col">
+        <div class="card shadow"> 
+          <div class="card-header border-0">
+            <div class="row align-items-center">
+              <div class="col"><h3 class="mb-0"> Light Table </h3></div>
+              <div class="col text-right">
+                <button type="button" class="btn btn-primary btn-sm">See all</button>
+              </div>
+            </div>
+          </div>
+          <table>
+            <thead>
+                <tr>
+                  <th>Barcode</th>
+                  <th>Name</th>
+                  <th>Qty</th>
+                  <th>Cost</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in tableData" :key="item.GoodsID">
+                  <th>{{ item.GoodsBarcode }}</th>
+                  <td>{{ item.GoodsName }}</td>
+                  <td>{{ item.GoodsQty }}</td>
+                  <td>฿{{ item.GoodsCost }}</td>
+                </tr>
+              </tbody>
+          </table>
+          <div class="card-footer d-flex justify-content-end">
+              <ul class="pagination"> 
+              <!-- :class="[size && pagination-${size}`, align && `justify-content-${align}`]" -->
+              <li class="page-item prev-page">
+                <a class="page-link" aria-label="Previous" @click="prevPage">
+                  <span aria-hidden="true"><i class="fa fa-angle-left" aria-hidden="true"></i></span>
+                </a>
+              </li>
+              <li class="page-item" :class="{active: value === item}" :key="item" v-for="item in 20">
+                <a class="page-link" @click="changePage(item)">{{item}}</a>
+              </li>
+              <!-- <li class="page-item next-page" :class="{disabled: value === totalPages}">
+                <a class="page-link" aria-label="Next" @click="nextPage">
+                  <span aria-hidden="true"><i class="fa fa-angle-right" aria-hidden="true"></i></span>
+                </a>
+              </li> -->
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <v-card class="mx-auto shadow">
+      <v-card-title>
+        <v-row no-gutters class="header">
+        <v-col>
+          <span>รายการสินค้า</span>
+        </v-col>
+        <v-col class="text-right">
+          <v-btn outlined color="indigo">{{ langGoods.ActionNew }}</v-btn>
+        </v-col>
+      </v-row>
+      </v-card-title>
+      <v-simple-table :dense="false" :fixed-header="true" :height="450">
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th>Barcode</th>
+              <th>Name</th>
+              <th>Qty</th>
+              <th>Cost</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in tableData" :key="item.GoodsID">
+              <th>{{ item.GoodsBarcode }}</th>
+              <td>{{ item.GoodsName }}</td>
+              <td>{{ item.GoodsQty }}</td>
+              <td>฿{{ item.GoodsCost }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+      <v-card class="d-flex mb-6" flat tile>
+        <v-card-title class="ml-auto" outlined tile>
+            <v-pagination
+        v-model="page"
+        :length="4"
+        circle
+      ></v-pagination>
+        </v-card-title>
+      </v-card>
+    </v-card>
+    <ManageGoodsModal :dialogGoods="dialogGoods"/> -->
+  </div>
+</template>
+<script>
+  import ManageGoodsModal from '@/components/Shared/Modal/GoodsModal'
+
+  export default {
+    name: 'projects-table',
+    components: {
+      ManageGoodsModal
+    },
+    computed: {
+      langGoods () {
+        return this.$t('listGoods')
+      }
+    },
+    data() {
+      return {
+        dialogGoods: false,
+        page: 1,
+        tableData: [
+          {
+            GoodsBarcode: '45862565626',
+            GoodsName: 'Argon Design System',
+            GoodsQty: '20',
+            GoodsCost: '50,000'
+          },
+          {
+            GoodsBarcode: '124125124214124',
+            GoodsName: 'React Material Dashboard',
+            GoodsQty: '45',
+            GoodsCost: '2,500'
+          },
+          {
+            GoodsBarcode: '1845564844',
+            GoodsName: 'Black Dashboard',
+            GoodsQty: '14',
+            GoodsCost: '8,500'
+          },
+          {
+            GoodsBarcode: '1845564844',
+            GoodsName: 'Black Dashboard',
+            GoodsQty: '14',
+            GoodsCost: '8,500'
+          },
+          {
+            GoodsBarcode: '1845564844',
+            GoodsName: 'Black Dashboard',
+            GoodsQty: '14',
+            GoodsCost: '8,500'
+          },
+          {
+            GoodsBarcode: '1845564844',
+            GoodsName: 'Black Dashboard',
+            GoodsQty: '14',
+            GoodsCost: '8,500'
+          }
+        ]
+      }
+    }
+  }
+</script>
+<style scoped>
+  .shadow {
+    box-shadow: 0 0 2rem 0 rgba(136, 152, 170, 0.15) !important;
+    background-color: #fff;
+    background-clip: border-box;
+    border: 1px solid rgba(0, 0, 0, 0.05);
+    border-radius: 0.375rem;
+  }
+
+  .card-header {
+    margin-bottom: 0;
+    background-color: #fff;
+    border: 0 !important;
+    border-radius: calc(0.375rem - 1px) calc(0.375rem - 1px) 0 0;
+    width: 100%;
+    color: #525f7f;
+    font-weight: bold;
+  }
+
+  table {
+    width: 100%;
+    margin-bottom: 1rem;
+    color: #525f7f;
+    background-color: transparent;
+  }
+
+  .card table td, .card table th {
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+  }
+
+  table.align-items-center td, table.align-items-center th {
+    vertical-align: middle;
+  }
+
+  table thead th {
+    padding-top: 0.75rem;
+    padding-bottom: 0.75rem;
+    font-size: 0.65rem;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    border-bottom: 1px solid #e9ecef;
+    background-color: #f6f9fc;
+    color: #8898aa;
+    border-color: #e9ecef;
+  }
+
+  table th, table td {
+    padding: 1rem;
+    vertical-align: top;
+    border-top: 1px solid #e9ecef;
+    font-size: 0.8125rem;
+    white-space: nowrap;
+  }
+
+  .table-flush tbody tr:first-child td, .table-flush tbody tr:first-child th {
+    border-top: 0;
+  }
+
+  table th {
+    font-weight: 600;
+  }
+
+  /* table thead th {
+    font-size: 0.65rem !important;
+    text-transform: uppercase !important;
+    letter-spacing: 1px !important;
+    border-bottom: 1px solid #e9ecef !important;
+    color: #8898aa !important;
+    background-color: #f6f9fc !important;
+    border-color: #e9ecef !important;
+  }
+
+  table th {
+    font-weight: 600 !important;
+    text-align: inherit !important;
+  }
+
+  table td, table th {
+    padding-left: 1.5rem !important;
+    padding-right: 1.5rem !important;
+    font-size: 0.8125rem !important;
+    white-space: nowrap !important;
+    padding: 1rem !important;
+    vertical-align: top !important;
+    border-top: 1px solid #e9ecef !important;
+  }
+
+  table tbody tr:first-child th {
+    font-size: 0.875rem !important;
+  }
+
+  table tbody tr:first-child td, table tbody tr:first-child th {
+    border-top: 0 !important;
+  }
+
+  table tbody tr th {
+    color: #525f7f;
+  }
+
+  table tbody tr td, table tbody tr th {
+    border-bottom: 0 !important;
+  } */
+</style>
